f20a619b9775483a39d5fe861189d7985a984f2f
ONIBON Olakunle Afolabi <onibon.afolabi@chem.loc>
2026-02-12 12:26:45 +0100

    troisieme commit

--- GithTest/src/router/index.js
+++ GithTest/src/router/index.js
@@ -1,8 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AcceuilViews from '@/views/AcceuilViews.vue'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
 
+const routes = [
+  {
+    path: '/',
+    component: HomeView,
+    name: 'home'
+  },
+  {
+    path: '/acceuil',
+    component: AcceuilViews,
+    name: 'acceuil'
+  },
+  {
+    path: '/about',
+    component: AboutView,
+    name: 'about'
+  },
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
